762069880d32611a96859c4acd5ee91b7b8250f4
Thomas SOËTE <thomas.soete@corp.ovh.com>
2017-07-05 16:03:01 +0200

    Remove useless file
    Signed-off-by: Thomas SOËTE <thomas.soete@corp.ovh.com>

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BUILD_VERSION=${BUILD_VERSION:-"dev"}
-BUILD_TIME=`date +"%Y-%m-%dT%H:%M:%S%z"`
-GIT_TAG=`git describe --always --dirty=-wip`
-
-go get -v -d
-go build -ldflags "-X stash.ovh.net/framework/build.Version=$BUILD_VERSION -X stash.ovh.net/framework/build.Time=$BUILD_TIME -X stash.ovh.net/framework/build.GitTag=$GIT_TAG"
